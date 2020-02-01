7b6508e8649f9a7e287f93fdab4aa00d2e16b67a
Ju Miranda <60076637+heinjuei@users.noreply.github.com>
2020-01-31 21:20:55 -0300

    Add files via upload

---
+++ 11_condition_3_triangles.py
@@ -0,0 +1,26 @@
+# Triangle repeat commands
+# Scalene triangle: All sides have different lengths.
+# Isosceles triangle: Two sides the same length.
+# Equilateral triangle: All sides are equal.
+
+a = int(input("The length of side a = "))
+
+while a < 0:
+    print(int("Please insert a positive number:"))
+    
+b = int(input("The length of side b = "))
+
+while b < 0:
+    print(int("Please insert a positive number:"))
+
+c = int(input("The length of side c = "))
+while c < 0:
+    print(int("Please insert a positive number:"))
+
+if a != b and b != c and a != c:
+ print("This a scalene triangle.")
+elif a == b and b == c:
+ print("This is an equilateral triangle.")
+else:
+ print("This is an isosceles triangle.")
+
