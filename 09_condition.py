67bb9bdc1a7e790f524aef4d9007f2bb809d1350
Ju Miranda <60076637+heinjuei@users.noreply.github.com>
2020-01-31 21:22:26 -0300

    Updating err0rs
    fix error (print)

--- 09_condition.py
+++ 09_condition.py
@@ -1,7 +1,6 @@
 # Collect string / test lehgnth
 
 a = str(input("Please enter a test string: "))
-
 if len(a) < 6:
     print("Your string is too short.")
     print("Please enter a string with at least 6 characters.")
