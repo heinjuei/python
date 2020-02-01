3878a575da00b976c22639dd5bcf643a42f279e5
Ju Miranda <60076637+heinjuei@users.noreply.github.com>
2020-01-31 21:19:27 -0300

    Update and rename 10_repeat_commands_2.py to 10_condition_2.py

--- 10_condition_2.py
+++ 10_condition_2.py
@@ -1,9 +1,11 @@
 # Prompt user to enter number / test if even or odd
 
-a = input("Please enter an integer: ")
+a = int(input("Please enter an integer: "))
 number = int(a)
 
 if number % 2 == 0:
 	print("Your number is even.")
 else:
-	print("Your number is idd.")
+	print("Your number is odd.")
+
+
