cf04676c41878f84a5c95e165cbafdec167dc74e
Ju Miranda <60076637+heinjuei@users.noreply.github.com>
2020-02-23 16:50:11 -0300

    Create 16_Linear_Lists.py
    useful functions and actions with lists
    [ordering groups]

---
+++ 16_Linear_Lists.py
@@ -0,0 +1,20 @@
+# Concatenation
+numbers = [1, 2, 3]
+letters = ['a', 'b', 'c']
+numbers + letters
+letters + numbers
+numbers
+letters
+dir(numbers)
+help(numbers.reverse)
+#reverse function
+numbers.reverse()
+numbers
+#pop function = removes the last one
+numbers.pop
+numbers
+# insert function = add item in order
+numbers.insert(2, 1)
+numbers
+# showing item position
+numbers.index(1)
